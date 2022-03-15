fdb9a8e3f932c63bc08b0f21ba4915725b95007c
denis.inpe <denis.inpe@rlogin01.cm.cluster>
2022-03-15 16:26:07 -0500

    updates

--- tools/qas_eval/getInfo.bash
+++ tools/qas_eval/getInfo.bash
@@ -1,9 +1,16 @@
 #!/bin/bash
-rm all.type.txt
-for f in *.F
+DIROUT=qas_out/$(date +'%Y%m%d-%H%m%S')
+mkdir -p ${DIROUT}
+
+DINCORE_PATH=$1
+TOOLS="/home/qas_files/tools"
+export LD_LIBRARY_PATH="${TOOLS}/graphviz/local/lib:${LD_LIBRARY_PATH}"
+for f in $(find $DINCORE_PATH -type f \( -iname \*.f90 -o -iname \*.F90 \))
 do
  echo "Processing $f"
- fortran-src $f -S -F 90 --show-make-graph > $f.dot
- fortran-src $f -t -F 90 >> all.type.txt
- dot -Tpng $f.dot -o $f.png
+ $TOOLS/fortran-src $f -S -F 90 --show-make-graph > $f.dot
+ $TOOLS/fortran-src $f -t -F 90 >> $DIROUT/all.type.txt
+ $TOOLS/graphviz/bin/dot -Tsvg $f.dot -o $f.svg
 done
+
+python3 ${HOME}/monan/tools/qas_eval/check.py $DINCORE_PATH
--- tools/qas_eval/run_eval.sh
+++ tools/qas_eval/run_eval.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sif_dir=$1
+export SINGULARITY_TMPDIR=$sif_dir/singularity_tmpdir
 singularity exec --bind ${sif_dir}:$HOME ${sif_dir}/qas_eval.sif ${HOME}/monan/tools/qas_eval/getInfo.bash "${HOME}//DinCore/GFDL_atmos_cubed_sphere/model/" 
+
+singularity shell --bind ${sif_dir}:$HOME ${sif_dir}/qas_eval.sif
