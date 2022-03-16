c7ba50b25e986f00033048121ee4de6638540b39
denis.inpe <denis.inpe@rlogin01.cm.cluster>
2022-03-16 16:55:56 -0500

    adicionando report do FortranAnalyser

--- tools/qas_eval/run_eval.sh
+++ tools/qas_eval/run_eval.sh
@@ -5,4 +5,4 @@ MODEL="fv3"
 
 export SINGULARITY_TMPDIR=$sif_dir/singularity_tmpdir
 singularity exec --bind ${sif_dir}:$HOME ${sif_dir}/qas_eval.sif ${HOME}/monan/tools/qas_eval/getInfo.bash "${HOME}//DinCore/GFDL_atmos_cubed_sphere/model/" 
-singularity exec --bind ${sif_dir}:$HOME ${sif_dir}/qas_eval.sif /home/qas_files/tools/jdk-17.0.2/bin/java -cp $HOME/monan/tools/qas_eval/FortranAnalyser-v2.0/target/FortranAnalyser.jar es.uvigo.esei.ephyslab.fortrananalyser.Principal "pt" "${HOME}/DinCore/GFDL_atmos_cubed_sphere/model/" "${HOME}/qas_out/QualityReport_${MODEL}.pdf" "NOGUI"
+singularity exec --bind ${sif_dir}:$HOME ${sif_dir}/qas_eval.sif /home/qas_files/tools/jdk-17.0.2/bin/java -cp  /home/qas_files/tools/FortranAnalyser.jar es.uvigo.esei.ephyslab.fortrananalyser.Principal "pt" "${HOME}/DinCore/GFDL_atmos_cubed_sphere/model/" "${HOME}/qas_out/QualityReport_${MODEL}.pdf" "NOGUI"
